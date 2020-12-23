40c860b51b706c2711c057f55e3fc9e557078220
764091925 <764091925@qq.com>
2020-12-23 12:13:02 +0800

    zernike矩亚像素的正式实现

--- Project1/Main_Prog_2.cpp
+++ Project1/Main_Prog_2.cpp
@@ -18,7 +18,7 @@ string Load_Path_2nd_8 = "G:\\Pictures\\Test For Programming\\eye.jpg"; // subPi
 //#define USE_ALL
 #ifdef USE_ALL
 string LoadPath_Msi_0 = "F:\\Pictures\\Test For Programming\\DSC_8967.jpg";
-string LoadPath_Msi_1 = "F:\\Pictures\\Test For Programming\\eye.jpg";
+
 string LoadPath_Msi_2 = "F:\\Pictures\\Test For Programming\\DSC_15774.jpg";
 string LoadPath_Msi_3 = "F:\\Pictures\\Test For Programming\\DSC_15774-4.jpg";
 string LoadPath_Msi_4 = "F:\\Pictures\\Test For Programming\\天山天池拼.jpg";
@@ -30,7 +30,8 @@ string LoadPath_Msi_9 = "F:\\Pictures\\Test For Programming\\gpgpu.png";
 string LoadPath_Msi_A = "F:\\Pictures\\Test For Programming\\噪声测试图像.png"; 
 
 #endif
-string LoadPath_Msi_B = "F:\\Pictures\\Test For Programming\\FreqFilterSrc.png";  
+string LoadPath_Msi_1 = "F:\\Pictures\\Test For Programming\\eye.jpg";
+string LoadPath_Msi_B("F:\\Pictures\\Test For Programming\\FreqFilterSrc.png");  
 string LoadPath_Msi_C = "F:\\Pictures\\Test For Programming\\1.jpg";  
 
 /*-----------------------------------------------------------------------------------------------------------------------------------*/
@@ -66,7 +67,8 @@ int main()
 	namedWindow(window_name, WINDOW_AUTOSIZE);
 
 	//原始图像组读取	
-	SRC_2nd = imread(LoadPath_Msi_B, IMREAD_GRAYSCALE); cout << SRC_2nd.type();
+	SRC_2nd = imread(LoadPath_Msi_1, IMREAD_GRAYSCALE);
+	cout << SRC_2nd.type();
 	if (!SRC_2nd.data)
 	{
 		cout << "读取失败" << endl;
@@ -74,6 +76,52 @@ int main()
 	}
 	imshow(window_name, SRC_2nd);
 	waitKey(0);
+ 
+
+	//以下是亚像素检测实现的前置处理
+	Temp_Array.push_back(SRC_2nd);
+
+	Temp_Buffer = Gaosi_双边(Temp_Array[0]); //高斯滤波+锐化
+	equalizeHist(Temp_Buffer,Temp_Buffer);
+	GaussianBlur(Temp_Buffer, Temp_Buffer, Size(7, 7), 1);
+	imshow(window_name, Temp_Buffer);
+	waitKey(0);
+	Temp_Array.push_back(Temp_Buffer);
+
+	Canny(Temp_Array[1], Temp_Buffer, 30, 150); //canny边缘检测
+	imshow(window_name, Temp_Buffer);
+	waitKey(0);
+	Temp_Array.push_back(Temp_Buffer);
+
+	vector<vector<Point>> Contours_Dected, Contours_Seletced;
+	vector<Point> Contours_for_SubPixCont;
+	vector<Point2d> Contours_subPix_result;
+	Moments 矩;
+	findContours(Temp_Array[2], Contours_Dected, RETR_EXTERNAL, CHAIN_APPROX_NONE, Point());  //圆形特征初步定位	
+	for (int i = 0; i < Contours_Dected.size(); i++)
+	{
+		size_t k = Contours_Dected[i].size();
+		if (k < 1000 && k > 700)
+		{
+			矩 = moments(Contours_Dected[i]);
+			if (abs(矩.nu11) < 0.001)
+			{
+				//判定归一化的高阶中心距作为判断标准，可能四个三阶归一化中心矩均为极小数10e-4级别可认为在x、y方向均不存在偏向性，可认为该形状关于X，Y轴均对称		
+				Contours_Seletced.push_back(Contours_Dected[i]);
+			}
+		}
+	}
+	if (Contours_for_SubPixCont.size() == 0) { throw out_of_range("没有符合条件的素材边缘!"); return -1; }
+	Contours_for_SubPixCont = Contours_Seletced[0];
+	cvtColor(Temp_Array[0], Temp_Buffer, COLOR_GRAY2BGR);
+	drawContours(Temp_Buffer, Contours_Seletced, -1, Scalar(0, 0, 255));
+	imshow(window_name, Temp_Buffer);
+	waitKey(0);
+	Temp_Array.push_back(Temp_Buffer); //指针问题未明确，注意传输地址导致vector容器中内容一致的可能
+
+	//亚像素边缘检测代码
+	Contours_subPix_result = SubPixel_Contours_Cgyt(Temp_Array[0], Contours_for_SubPixCont, 2.0);
+
 
 	//HistogramCGYT(SRC_2nd);
 	//FT_CGYT(SRC_2nd, Temp_Buffer);
